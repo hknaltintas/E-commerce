574344f29bf6dd4bfabbe332d34de6df4cd40933
Hakan Altintas <hhakanaltintas@gmail.com>
2023-04-10 21:19:02 +0300

    Delete console log

--- src/componenets/pages/Products/index.jsx
+++ src/componenets/pages/Products/index.jsx
@@ -17,13 +17,12 @@ function Products() {
     status,
   } = useInfiniteQuery(["products"], fetchproductList, {
     getNextPageParam: (lastGroup, allGroups) => {
-      console.log("allgrouplength", allGroups.length);
       const morePagesExist = lastGroup.length === 12;
 
       if (!morePagesExist) {
         return;
       }
-      
+
       return allGroups.length + 1;
     },
   });
@@ -31,7 +30,7 @@ function Products() {
   if (status === "loading") return "Loading...";
 
   if (status === "error") return "An error has occurred: " + error.message;
-  console.log("data", data);
+
   return (
     <div>
       <Grid templateColumns="repeat(3, 1fr)" gap={3}>
